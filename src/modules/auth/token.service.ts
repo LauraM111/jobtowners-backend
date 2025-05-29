21f7fcf8323cac1fc152d238b19d6e426bc22537
Mohit Sharma <mohi@mediahubster.com>
2025-05-29 12:39:17 +0530

    working on the reset-password

--- src/modules/auth/token.service.ts
+++ src/modules/auth/token.service.ts
@@ -226,9 +226,9 @@ export class TokenService {
     
     const user = await this.userService.findById(tokenRecord.userId);
     
-    if (!user.isEmailVerified) {
-      throw new BadRequestException('Email not verified');
-    }
+    // if (!user.isEmailVerified) {
+    //   throw new BadRequestException('Email not verified');
+    // }
     
     return user;
   }
